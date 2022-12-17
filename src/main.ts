8b50764a143c2f27dd026728521977d6c32ebb1b
huzhengen <huzhengen@163.com>
2022-12-17 16:50:23 +0800

    Update camera

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 
 const scene = new THREE.Scene() // 场景
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000) // 透视摄像机
-camera.position.set(0.0, 0.2, 1.0); // 设置相机的位置
+camera.position.set(0.0, 0.4, 1.0); // 设置相机的位置
 const renderer = new THREE.WebGLRenderer({ antialias: true }) // 渲染器
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement) // 放到页面里
